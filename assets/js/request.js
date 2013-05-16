b840c8bbc097e22c5c9debb59fa3516cb2e1977e
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-05-17 01:24:35 +0300

    Stubs for future modules

---
+++ assets/js/request.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/16/13
+ * Time: 4:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ assets/js/storage.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/16/13
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
