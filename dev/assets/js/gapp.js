101a85ba3d055f87cb8f8508466ccc39dd5e361e
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-05-17 21:53:56 +0300

    Auth messages demo

--- dev/assets/js/gapp.js
+++ dev/assets/js/gapp.js
@@ -1,3 +1,66 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 7:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function (window, document, $, gapp, undefined) {
+
+    function Module() {
+
+        var token,
+            loginCallback = function (authResult) {
+                $('.error-message').addClass('hidden');
+                if(authResult.error){
+                   switch(authResult.error.code){
+                       case 401:
+                           $('#unauth').removeClass('hidden');
+                           break;
+                       case 500:
+                           $('#invalidreq').removeClass('hidden');
+                           break;
+                   }
+                }
+
+                token = authResult.access_token;
+                gapp.router.indexPage();
+            },
+            login = function () {
+                var $login = $('.input_login'),
+                    $password = $('.input_password'),
+
+                    loginVal = $login.val(),
+                    passwordVal = $password.val(),
+
+                    $loginContainer = $('.form-container-login'),
+                    $passwordContainer = $('.form-container-password');
+
+                $loginContainer.removeClass('form-container__error');
+                $passwordContainer.removeClass('form-container__error');
+
+                if (loginVal === '') {
+                    $loginContainer.addClass('form-container__error');
+                    return;
+                }
+                if (passwordVal === '') {
+                    $passwordContainer.addClass('form-container__error');
+                    return;
+                }
+
+
+                gapp.request.authenticate(loginVal, passwordVal, loginCallback);
+            };
+
+        return {
+            login:login
+        };
+    }
+
+    window.gapp.auth = new Module();
+
+
+})(window, document, jQuery, gapp || {});
 (function (window, document, ymaps, gapp, undefined) {
 
     function Module(){
@@ -89,6 +152,74 @@
  * To change this template use File | Settings | File Templates.
  */
 
+(function (window, document, $, gapp, undefined) {
+
+    function Module() {
+
+        var authenticate = function (login, pasword, authCallback) {
+
+            var unAuthUserError = {
+                error:{
+                    code:401,
+                    error_message:"User unauthorized"
+                }
+            };
+
+            var incorrectParamsRequestError = {
+                error:{
+                    code:500,
+                    error_message:"Does not has all required fields"
+                }
+            };
+
+            var successResult = {
+                access_token : 'alr9wUGYBf4783nJSByfb4'
+            };
+
+            setTimeout(authCallback(successResult), 2000);
+        };
+
+        return {
+            authenticate:authenticate
+        };
+    }
+
+    window.gapp.request = new Module();
+
+})(window, document, jQuery, gapp || {});
+
+
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 9:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function (window, document, gapp, undefined) {
+
+    function Module() {
+
+        var loginPageUrl = '/login.html',
+            indexPageUrl = '/index.html',
+            loginPage = function () {
+                document.location = loginPageUrl;
+            },
+            indexPage = function () {
+                document.location = indexPageUrl;
+            };
+
+        return {
+            loginPage:loginPage,
+            indexPage:indexPage
+        };
+
+    }
+
+    window.gapp.router = new Module();
+
+})(window, document, gapp || {});
 /**
  * Created with JetBrains WebStorm.
  * User: Administrator
