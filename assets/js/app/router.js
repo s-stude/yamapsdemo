96a4c7d2554078d0c1f4f6842382a9645f6fccb3
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-05-17 21:51:44 +0300

    Adding simple router

---
+++ assets/js/app/router.js
@@ -0,0 +1,31 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 9:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function (window, document, gapp, undefined) {
+
+    function Module() {
+
+        var loginPageUrl = '/login.html',
+            indexPageUrl = '/index.html',
+            loginPage = function () {
+                document.location = loginPageUrl;
+            },
+            indexPage = function () {
+                document.location = indexPageUrl;
+            };
+
+        return {
+            loginPage:loginPage,
+            indexPage:indexPage
+        };
+
+    }
+
+    window.gapp.router = new Module();
+
+})(window, document, gapp || {});
