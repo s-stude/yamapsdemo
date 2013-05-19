aee2b1a91aa3a3121e8a39c4cb45f7b240bdff01
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-05-19 22:38:12 +0300

    Adding dummy ASP.NET MVC project for AJAX requests

---
+++ aspnet/aspnet/Controllers/ClientsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace aspnet.Controllers
+{
+    public class ClientVM
+    {
+        public string manager { get; set; }
+        public string client { get; set; }
+        public string address { get; set; }
+    }
+
+    public class ClientsController : Controller
+    {
+        public ActionResult ClientInfo()
+        {
+            Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            return Json(new
+                {
+                    client_list = new List<ClientVM>
+                        {
+                            new ClientVM
+                                {
+                                    client = "Иванов",
+                                    address = "крымских партизан 21",
+                                    manager = "Мэн1"
+                                },
+                            new ClientVM
+                                {
+                                    client = "Петров",
+                                    address = "крымских партизан 23",
+                                    manager = "Мэн2"
+                                },
+                            new ClientVM
+                                {
+                                    client = "Сидоров",
+                                    address = "крымских партизан",
+                                    manager = "Мэн3"
+                                },
+//                            new ClientVM
+//                                {
+//                                    Client = "",
+//                                    Address = "",
+//                                    manager = ""
+//                                },
+                        }
+                }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ aspnet/aspnet/Controllers/DmsbClientControler.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+
+namespace aspnet.Controllers
+{
+    public class DmsbClientController : Controller
+    {
+       public ActionResult Auth(int? error)
+        {
+            Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            if (error.HasValue && error.Value == 401)
+            {
+                return Json(new
+                    {
+                        error = new
+                            {
+                                code = 401,
+                                error_message = "User unauthorized"
+                            }
+                    }, JsonRequestBehavior.AllowGet);
+            }
+
+            if (error.HasValue && error.Value == 500)
+            {
+                return Json(new
+                    {
+                        error = new
+                            {
+                                code = 500,
+                                error_message = "Does not has all required fields"
+                            }
+                    }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new
+                {
+                    access_token = "alr9wUGYBf4783nJSByfb4"
+                }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
