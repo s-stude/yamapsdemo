37680f8c9c611a5c164b013ee925fc461cce074d
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-05-30 17:00:35 +0300

    Adding www-root sources

---
+++ www-root/assets/js/gapp.js
@@ -0,0 +1,411 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 10:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function (window, gapp, undefined) {
+
+    window.gapp.Client = function () {
+        var manager,
+            client,
+            address;
+
+        return {
+            manager:manager,
+            client:client,
+            address:address
+        };
+    };
+
+})(window, gapp || {});
+
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/16/13
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(window, document, gapp, undefined){
+
+    function AppStorage(){
+
+        var getValue = function(key){
+                return localStorage.getItem(key);
+            },
+            setValue = function(key, value){
+                localStorage.setItem(key, value);
+            };
+
+        return {
+            getValue: getValue,
+            setValue: setValue
+        };
+
+    }
+
+    window.gapp.appStorage = new AppStorage();
+
+})(window, document, gapp || {});
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 7:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function (window, document, $, gapp, undefined) {
+
+    function Auth() {
+
+        var token = function (tokenToSave) {
+                if (tokenToSave) {
+                    gapp.appStorage.setValue('access_token', tokenToSave);
+                }
+
+                return gapp.appStorage.getValue('access_token');
+            },
+            navigateIndexPageOrError = function (authResultPlain) {
+
+                var authResult = authResultPlain; // TODO: For debug
+//                var authResult = JSON.parse(authResultPlain);
+
+                if (authResult.error) {
+                    switch (authResult.error.code) {
+                        case 401:
+                            $('#unauth').removeClass('hidden');
+                            break;
+                        case 500:
+                            $('#invalidreq').removeClass('hidden');
+                            break;
+                    }
+                    return;
+                }
+
+                token(authResult.access_token);
+
+                gapp.router.indexPage();
+            },
+            showLogin404Message = function(){
+                $('#desthost404').removeClass('hidden');
+            },
+            showCustomLoginError = function(errorCode, errorText){
+                $('#customMessage').text(errorCode + ' ' + errorText);
+                $('#customMessage').removeClass('hidden');
+            },
+            handleLogin = function () {
+                var $login = $('.input_login'),
+                    $password = $('.input_password'),
+
+                    loginVal = $login.val(),
+                    passwordVal = $password.val(),
+
+                    $loginContainer = $('.form-container-login'),
+                    $passwordContainer = $('.form-container-password');
+
+                $('.error-message').addClass('hidden');
+
+                $loginContainer.removeClass('form-container__error');
+                $passwordContainer.removeClass('form-container__error');
+
+                if (loginVal === '') {
+                    $loginContainer.addClass('form-container__error');
+                    return;
+                }
+                if (passwordVal === '') {
+                    $passwordContainer.addClass('form-container__error');
+                    return;
+                }
+
+                gapp.request.authenticate(loginVal, passwordVal)
+                    .then(function (authResult) {
+                        navigateIndexPageOrError(authResult);
+                    })
+                    .fail(function (error) {
+                        if(error.status === 404){
+                            showLogin404Message();
+                        } else {
+                            showCustomLoginError(error.status, error.statusText);
+                        }
+                    });
+            };
+
+        return {
+            access_token:token,
+            login:handleLogin
+        };
+    }
+
+    window.gapp.auth = new Auth();
+
+
+})(window, document, jQuery, gapp || {});
+(function (window, document, $, gapp, undefined) {
+
+    function GeoLocator(){
+        var d = $.Deferred(),
+            isGeolocationApiSupported = (function () {
+                return 'geolocation' in window.navigator;
+            })(),
+
+            getCurrentPositionCallback = function (position) {
+                d.resolve([position.coords.latitude, position.coords.longitude]);
+            },
+
+            errorCallback = function(err){
+                // nothing to handle yet. Falling back to YA Map API
+                d.resolve([ymaps.geolocation.latitude, ymaps.geolocation.longitude]);
+            },
+
+            getCurrentPosition = function () {
+                if (isGeolocationApiSupported) {
+                    window.navigator.geolocation.getCurrentPosition(getCurrentPositionCallback, errorCallback);
+                } else{
+                    d.resolve([ymaps.geolocation.latitude, ymaps.geolocation.longitude]);
+                }
+
+                return d.promise();
+            };
+
+        return {
+            isGeolocationApiSupported:isGeolocationApiSupported,
+            getCurrentPosition:getCurrentPosition
+        };
+    }
+
+    gapp.geolocator = new GeoLocator();
+
+})(window, document, jQuery, gapp || {});
+(function (window, document, $, ymaps, gapp, undefined) {
+
+    function Main() {
+        var mapOnPage,
+            createMap = function (centerPosition) {
+                var
+                    refreshButton,
+                    buttonsToolBar = new ymaps.control.ToolBar();
+
+                mapOnPage = new ymaps.Map("map", {
+                    center:centerPosition,
+                    zoom:14
+                });
+
+                mapOnPage.geoObjects.add(
+                    new ymaps.Placemark(
+                        centerPosition,
+                        {
+                            iconContent:"Я",
+                            hintContent:"Вы тут",
+                            balloonContent:"Ваше местоположение"
+                        }
+                    )
+                );
+
+                mapOnPage.controls.add('smallZoomControl');
+
+                // Create Clients button
+                refreshButton = new ymaps.control.Button({
+                    data:{
+                        image:'assets/img/view_refresh.png',
+                        title:'Обновить точки на карте'
+                    }
+                }, {
+                    selectOnClick:false
+                });
+
+                refreshButton.events.add('click', function(){
+                    var clientsInfoPromise = gapp.request.getClients();
+                    $.when(clientsInfoPromise)
+                        .then(function (clients) {
+                            addClientsToMap(clients);
+                        })
+                        .fail(function (error) {
+                            debugger;
+                            alert(error);
+                        });
+                });
+
+//                refreshButton.events.add('select', function () {
+//                    clientsClusterer.add(friendsGeoObjects);
+//                });
+//                clientsButton.events.add('deselect', function () {
+//                    clientsClusterer.remove(friendsGeoObjects);
+//                });
+
+                buttonsToolBar.add(refreshButton);
+                mapOnPage.controls.add(buttonsToolBar);
+            },
+            createPoint = function (client) {
+                console.log('Geocoding - ' + client.client);
+
+                var geocodeAddress = ymaps.geocode(client.address, { results:1});
+
+                geocodeAddress.then(
+                    function (res) {
+                        console.log('Geocoded - ' + client.client);
+
+                        var first = res.geoObjects.get(0),
+                            coords = first.geometry.getCoordinates(); // TODO: save to storage;
+
+                        gapp.appStorage.setValue(client.address, coords);
+
+                        first.properties.set('balloonContentHeader', client.client);
+                        first.properties.set('balloonContentBody', client.manager);
+
+                        first.options.set('preset', 'twirl#redDotIcon');
+
+
+                        mapOnPage.geoObjects.each(function (geoObject) {
+
+                            var geoId = geoObject.properties.get('id');
+                            if (geoId == 'some id') {
+                                // do something
+
+                                return false;
+                            }
+                        });
+
+                        mapOnPage.geoObjects.add(first);
+                    });
+            },
+            addClientsToMap = function (clients) {
+                var i, client;
+
+                for (i = 0; i < clients.client_list.length; i++) {
+                    client = clients.client_list[i];
+
+                    console.log('Iterating - ' + client.client);
+                    createPoint(client);
+                }
+            },
+            initMap = function () {
+
+                var positionPromise = gapp.geolocator.getCurrentPosition();
+                var clientsInfoPromise = gapp.request.getClients();
+
+                $.when(positionPromise, clientsInfoPromise)
+                    .then(function (currentPosition, clients) {
+                        createMap(currentPosition);
+                        addClientsToMap(clients);
+                    })
+                    .fail(function (error) {
+                        debugger;
+                        alert(error);
+                    });
+            };
+
+
+        return {
+            onInit:initMap
+        };
+    }
+
+    gapp.main = new Main();
+
+})(window, document, jQuery, ymaps, gapp || {});
+
+
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/16/13
+ * Time: 4:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function (window, document, $, gapp, undefined) {
+
+    function Request() {
+
+        var loginUrl = '/dmsbclient/auth',
+            clientsUrl = '/dmsbclient/clientinfo',
+            authenticate = function (login, password, authCallback) {
+                /*
+                 *
+                 * POST /dmsbclient/auth
+                 *
+                 * PARAMS:
+                 * login
+                 * password
+                 *
+                 * */
+
+                var d = $.Deferred();
+
+                var ajaxPost = $.ajax({
+                    url:loginUrl,
+                    data:JSON.stringify({login:login, password:password}),
+                    type:'POST' // TODO: This should be POST
+                }).done(function (result) {
+                    debugger;
+                    d.resolve(result);
+                }).fail(function (error) {
+                    debugger;
+                    d.reject(error);
+                });
+
+                return d.promise();
+            },
+            getClients = function () {
+                var d = $.Deferred();
+
+                var access_token = gapp.auth.access_token();
+                $.ajax({
+                    url: clientsUrl,
+                    data: { access_token : access_token },
+                    type: "POST" // TODO: POST in PROD
+                }).done(function(result){
+                        d.resolve(result);
+                    }).fail(function(error){
+                        d.reject(error);
+                    });
+
+                return d.promise();
+            };
+
+        return {
+            authenticate:authenticate,
+            getClients:getClients
+        };
+    }
+
+    window.gapp.request = new Request();
+
+})(window, document, jQuery, gapp || {});
+
+
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 5/17/13
+ * Time: 9:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function (window, document, gapp, undefined) {
+
+    function Router() {
+
+        var loginPageUrl = 'login.html',
+            indexPageUrl = 'index.html',
+            loginPage = function () {
+                document.location = loginPageUrl;
+            },
+            indexPage = function () {
+                document.location = indexPageUrl;
+            };
+
+        return {
+            loginPage:loginPage,
+            indexPage:indexPage
+        };
+
+    }
+
+    window.gapp.router = new Router();
+
+})(window, document, gapp || {});
